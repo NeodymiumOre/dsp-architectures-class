8e78329e0354f5eb8eba616c963e07d8045cc007
NeodymiumOre <maciejbarna@gmail.com>
2023-12-02 16:54:05 +0100

    Added filtering for lab5 signal

--- lab5-python/main.py
+++ lab5-python/main.py
@@ -19,6 +19,13 @@ def read_wav_file(file_path):
         sample_rate = wave_file.getframerate()
     return signal, sample_rate
 
+def plot_frequency_spectrum(freq, fft_result):
+    plt.plot(freq, fft_result)
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude Spectrum')
+    plt.title('Frequency Spectrum')
+    plt.show()
+
 
 if __name__ == "__main__":
 
@@ -29,3 +36,23 @@ if __name__ == "__main__":
 
     # plot the waveform
     plot_waveform(signal, sample_rate)
+
+    # perform fft
+    n = len(signal)
+    # print(sample_rate)
+    freq = np.fft.fftfreq(n, d=1/sample_rate)
+    fft_result = np.fft.fft(signal)
+
+    # plot the fft
+    plot_frequency_spectrum(freq, fft_result)
+
+    # do the filtering
+    cut_off_freq = 170
+    fft_filtered = fft_result.copy()
+    fft_filtered[np.abs(freq) < cut_off_freq] = 0
+
+    plot_frequency_spectrum(freq, fft_filtered)
+
+    # perform inverse fft
+    signal_filtered = np.fft.ifft(fft_filtered)
+    plot_waveform(signal_filtered, sample_rate)
